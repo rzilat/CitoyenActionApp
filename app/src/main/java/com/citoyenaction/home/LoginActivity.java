da11c19a94a1f82d4d0623f6e2b769cbca348b63
redao <redaovish200@hotmail.com>
2020-05-27 22:23:59 +0000

    initial commit for citoyenactionApp

---
+++ app/src/main/java/com/citoyenaction/home/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.citoyenaction.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/citoyenaction/home/MainActivity.java
@@ -0,0 +1,25 @@
+package com.citoyenaction.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        Button loginButton=findViewById(R.id.loginButton);
+        startActivity(intent);
+    }
+}
